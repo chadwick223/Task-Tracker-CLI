e022464ea7f25f77b83b6eaead02bfc09e3b8bfc
satvik <satvikshukla955923@gmail.com>
2025-11-26 23:17:11 +0530

    addition of task

--- Task_cli.py
+++ Task_cli.py
@@ -1,6 +1,6 @@
 import json
 import os
-
+from datetime import datetime
 TASK_FILE="task.json"
 
 
@@ -13,9 +13,43 @@ def load_task():
         try :
             return json.load(file)
         except json.JSONDecodeError :
-            return [] 
-tasks=load_task()
-for t in tasks:
-    print (t)
+            return []
+        
+# load_task will return me the list of dictionary
 
-print("script startedT")
+def add_tasks(description,tasks):
+    # if im creating a task , i need its id, its description, its status. at the time of creation, the status should be to do
+    # and at the time of creation it should also have created at and updated at
+    # can there be a way that as soon as i create task, or call create task. then automatically
+    # {id , description,status,created at,  updated at, } keys are generated, and then i give input in an array and all those
+    # element are mapped with keys
+    if not tasks:
+        taskid=1
+    else:
+        heighest=0
+        for task in tasks:
+            if task["id"]>heighest:
+                heighest = task["id"]
+        taskid=heighest+1
+    timestamp=datetime.now().strftime("%y/%m/%d %I:%M %p")
+    new_task={
+        "id":taskid,
+        "description":description,
+        "status":"TODO",
+        "createdAt":timestamp,
+        "updatedAt":timestamp
+    }
+    tasks.append(new_task)
+    save_tasks(tasks)
+    #first thing i need to check is if that task is present in my jason or not 
+    # if it is present , then I'll return some thing , else i'll add
+def save_tasks(tasks):
+    with open(TASK_FILE,"w") as file:
+        json.dump(tasks,file,indent=4)
+     
+tasks=load_task()
+
+add_tasks("outing",tasks)
+tasks=load_task()
+print(tasks)
+#print("script startedT")
